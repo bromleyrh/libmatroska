e9cac46e05df5f64847ca888bcf59b7d96548aa1
Richard Bromley <bromleyrh@gmail.com>
2022-07-23 16:31:57 -0700

    Commit initial work (19)
    Commit UUID: 6f67046c-b979-4eba-a37b-e43598f38015

--- trunk/schema_parse.c
+++ trunk/schema_parse.c
@@ -2,109 +2,81 @@
  * schema_parse.c
  */
 
-#include <expat.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+#include <libxml/xmlschemas.h>
 
-#include <errno.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-#ifdef XML_UNICODE_WCHAR_T
-#include <wchar.h>
-#endif
-
-#ifdef XML_LARGE_SIZE
-#define PRI_XML_LINENO "llu"
-#else
-#define PRI_XML_LINENO "lu"
-#endif
-
-#ifdef XML_UNICODE_WCHAR_T
-#define PRI_XML_CHAR "ls"
-#else
-#define PRI_XML_CHAR "s"
-#endif
-
-static const char tabs[] = "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t";
-
-#define TABS(level) (&tabs[sizeof(tabs) - 1 - (level)])
-
-static void XMLCALL start_element_cb(void *, const XML_Char *,
-                                     const XML_Char **);
-static void XMLCALL end_element_cb(void *, const XML_Char *);
-
-static void XMLCALL
-start_element_cb(void *userData, const XML_Char *name, const XML_Char **atts)
-{
-    int *level = userData;
-
-    (void)atts;
-
-    printf("%s%" PRI_XML_CHAR " {\n", TABS(*level), name);
-
-    ++*level;
-}
-
-static void XMLCALL
-end_element_cb(void *userData, const XML_Char *name)
-{
-    int *level = userData;
-
-    (void)name;
-
-    --*level;
-
-    printf("%s}\n", TABS(*level));
-}
 
 int
 main(int argc, char **argv)
 {
-    enum XML_Status res;
-    int level = 0;
-    int status = EXIT_FAILURE;
-    XML_Parser parser;
+    const char *schemaf;
+    int ret, status;
+    xmlDocPtr doc;
+    xmlSchemaParserCtxtPtr ctx;
+    xmlSchemaPtr schema;
+    xmlSchemaValidCtxtPtr vctx;
 
-    (void)argc;
-    (void)argv;
-
-    parser = XML_ParserCreate(NULL);
-    if (parser == NULL)
+    if (argc < 2) {
+        fputs("Must specify XML schema path\n", stderr);
         return EXIT_FAILURE;
+    }
+    schemaf = argv[1];
 
-    XML_SetElementHandler(parser, &start_element_cb, &end_element_cb);
-    XML_SetUserData(parser, &level);
+    LIBXML_TEST_VERSION
 
-    for (;;) {
-        char buf[4096];
-        int is_final;
-        size_t ret;
+    doc = xmlParseFile(schemaf);
+    if (doc == NULL)
+        goto err1;
 
-        ret = fread(buf, 1, sizeof(buf), stdin);
-        is_final = ret == 0;
-        if (is_final && !feof(stdin)) {
-            fprintf(stderr, "Error reading: %s\n", strerror(errno));
-            break;
-        }
+    ctx = xmlSchemaNewDocParserCtxt(doc);
+    if (ctx == NULL)
+        goto err2;
 
-        res = XML_Parse(parser, buf, ret, is_final);
-        if (res != XML_STATUS_OK) {
-            fprintf(stderr, "%" PRI_XML_CHAR " at line %" PRI_XML_LINENO "\n",
-                    XML_ErrorString(XML_GetErrorCode(parser)),
-                    XML_GetCurrentLineNumber(parser));
-            break;
-        }
+    schema = xmlSchemaParse(ctx);
+    if (schema == NULL)
+        goto err3;
 
-        if (is_final) {
-            status = EXIT_SUCCESS;
-            break;
-        }
+/*    xmlSchemaDump(stdout, schema);
+*/
+    vctx = xmlSchemaNewValidCtxt(schema);
+    if (vctx == NULL)
+        goto err4;
+
+    ret = xmlSchemaValidateFile(vctx, "-", 0);
+    if (ret == 0)
+        status = EXIT_SUCCESS;
+    else {
+        status = EXIT_FAILURE;
+        fprintf(stderr, "%s\n",
+                ret == -1
+                ? "Error validating XML document" : "XML document invalid");
     }
 
-    XML_ParserFree(parser);
+    xmlSchemaFreeValidCtxt(vctx);
+
+    xmlSchemaFree(schema);
+
+    xmlSchemaFreeParserCtxt(ctx);
+
+    xmlFreeDoc(doc);
+    xmlCleanupParser();
 
     return status;
+
+err4:
+    xmlSchemaFree(schema);
+err3:
+    xmlSchemaFreeParserCtxt(ctx);
+err2:
+    xmlFreeDoc(doc);
+    xmlCleanupParser();
+err1:
+    fprintf(stderr, "Error parsing %s\n", schemaf);
+    return EXIT_FAILURE;
 }
 
 /* vi: set expandtab sw=4 ts=4: */
