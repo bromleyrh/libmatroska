ff410d84003a2af016a1a4acc6ed5cdf945754e3
Richard Bromley <bromleyrh@gmail.com>
2022-08-17 23:59:56 -0700

    Commit initial work (53)
    Commit UUID: efe0d7b0-14f4-403f-84f9-13afb8294e5e

--- trunk/include/matroska.h
+++ trunk/include/matroska.h
@@ -15,9 +15,23 @@
 extern "C" {
 #endif
 
+#define LIST_BLOCK_HDR_FIELDS() \
+    _X(TRACKNO,     8) \
+    _X(TIMESTAMP,   2) \
+    _X(FLAGS,       1)
+
+#define _X(nm, len) \
+enum { \
+    BLOCK_HDR_##nm##_LEN = len \
+};
+LIST_BLOCK_HDR_FIELDS()
+#undef _X
+
 struct matroska_state {
     int     block_hdr;
-    char    hdr_buf[8];
+#define _X(nm, len) + len
+    char    hdr_buf[LIST_BLOCK_HDR_FIELDS()];
+#undef _X
     size_t  hdr_len;
     size_t  hdr_sz;
 };
--- trunk/matroska.c
+++ trunk/matroska.c
@@ -14,6 +14,17 @@
 
 #include <sys/param.h>
 
+#define BLOCK_FLAG_KEYFRAME 1
+#define BLOCK_FLAG_INVISIBLE 16
+#define BLOCK_FLAG_DISCARDABLE 128
+
+#define BLOCK_FLAG_LACING_SHIFT 5
+#define BLOCK_FLAG_LACING_MASK (3 << BLOCK_FLAG_LACING_SHIFT)
+
+#define BLOCK_FLAG_LACING_XIPH (1 << BLOCK_FLAG_LACING_SHIFT)
+#define BLOCK_FLAG_LACING_FIXED_SIZE (2 << BLOCK_FLAG_LACING_SHIFT)
+#define BLOCK_FLAG_LACING_EBML (3 << BLOCK_FLAG_LACING_SHIFT)
+
 int
 matroska_tracknumber_handler(const char *val, const void *buf, size_t len,
                              void *ctx)
@@ -29,6 +40,10 @@ matroska_tracknumber_handler(const char *val, const void *buf, size_t len,
 
     return 0;
 }
+
+#define BLOCK_HDR_FIXED_LEN (BLOCK_HDR_TIMESTAMP_LEN + BLOCK_HDR_FLAGS_LEN)
+
+#define FLAG_VAL(flags, which) (!!((flags) & BLOCK_FLAG_##which))
 
 int
 matroska_simpleblock_handler(const char *val, const void *buf, size_t len,
@@ -54,6 +69,7 @@ matroska_simpleblock_handler(const char *val, const void *buf, size_t len,
         if (err)
             return err;
         state->hdr_len = 0;
+        state->hdr_sz += BLOCK_HDR_FIXED_LEN;
 
         state->block_hdr = 2;
     }
@@ -68,20 +84,53 @@ matroska_simpleblock_handler(const char *val, const void *buf, size_t len,
     state->hdr_sz -= sz;
 
     if (state->hdr_sz == 0) {
+        uint8_t flags;
         uint64_t trackno;
+        union {
+            int16_t val;
+            char    bytes[2];
+        } timestamp;
+
+        static const char *const lacing_typemap[] = {
+            [0]                             = "none",
+            [BLOCK_FLAG_LACING_XIPH]        = "Xiph",
+            [BLOCK_FLAG_LACING_FIXED_SIZE]  = "fixed-size",
+            [BLOCK_FLAG_LACING_EBML]        = "EBML"
+        };
 
         err = vint_to_u64(state->hdr_buf, &trackno, &sz);
         if (err)
             return err;
-        if (sz != state->hdr_len)
+        if (sz != state->hdr_len - BLOCK_HDR_FIXED_LEN)
             return -EILSEQ;
 
-        fprintf(stderr, "Track number %" PRIu64 "\n", trackno);
+        timestamp.bytes[0] = state->hdr_buf[sz+1];
+        timestamp.bytes[1] = state->hdr_buf[sz];
+
+        flags = state->hdr_buf[sz + BLOCK_HDR_TIMESTAMP_LEN];
+
+        fprintf(stderr, "Track number %" PRIu64 "\n"
+                        "Timestamp %" PRIi16 "\n"
+                        "Flags %" PRIu8 "\n"
+                        "Keyframe %d\n"
+                        "Invisible %d\n"
+                        "Discardable %d\n"
+                        "Lacing type %s\n",
+                trackno, timestamp.val, flags,
+                FLAG_VAL(flags, KEYFRAME),
+                FLAG_VAL(flags, INVISIBLE),
+                FLAG_VAL(flags, DISCARDABLE),
+                lacing_typemap[(flags & BLOCK_FLAG_LACING_MASK)
+                               >> BLOCK_FLAG_LACING_SHIFT]);
 
         state->block_hdr = 1;
     }
 
     return 0;
 }
+
+#undef BLOCK_HDR_FIXED_LEN
+
+#undef FLAG_VAL
 
 /* vi: set expandtab sw=4 ts=4: */
